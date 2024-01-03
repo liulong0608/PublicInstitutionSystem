d644dce82d0313907f3d09d0f7d6f1ccba9bc423
Echo <liulong@3mencn.com>
2024-01-03 10:11:13 +0800

    v1.3.4-beta1.3.3

--- public/common/browser.py
+++ public/common/browser.py
@@ -4,6 +4,7 @@
 **    @author           Echo
 **    @EditTime         2023/12/11
 """
+import sys
 from typing import *
 
 from selenium.webdriver.chrome.service import Service
@@ -13,20 +14,41 @@ from selenium import webdriver
 
 from public.common.exceptions import UnsupportedBrowserException
 
+system_driver = sys.platform
 
 def select_browser(browser=globalparam.browser.lower()):
     dc = {'platform': 'ANY', 'browserName': 'chrome', 'version': '', 'javascriptEnabled': True}
     dr = None
-    if browser == "firefox":
-        dr = webdriver.Firefox()
-    elif browser == "chrome":
-        options = webdriver.ChromeOptions()
-        options.add_argument('ignore-certificate-errors')
-        service = Service(globalparam.driver_path)
-        dr = webdriver.Chrome(options=options, service=service)
-    elif browser == "edge":
-        dr = webdriver.Edge()
+    if system_driver.lower() == "win32":
+        if browser == "firefox":
+            dr = webdriver.Firefox()
+        elif browser == "chrome":
+            options = webdriver.ChromeOptions()
+            options.add_argument('ignore-certificate-errors')
+            service = Service(globalparam.driver_path)
+            dr = webdriver.Chrome(options=options, service=service)
+        elif browser == "edge":
+            dr = webdriver.Edge()
+        else:
+            raise UnsupportedBrowserException(f"Browser {browser} is not supported.")
+        return dr
+    elif system_driver.lower() == "linux":
+        if browser == "firefox":
+            dr = webdriver.Firefox()
+        elif browser == "chrome":
+            print("判断系统为linux")
+            options = webdriver.ChromeOptions()
+            options.add_argument('no-sandbox')
+            options.add_argument("headless")  # 配置无头浏览器
+            options.add_argument('disable-dev-shm-usage')
+            options.add_argument('disable-gpu')  # 需要加上这个属性来规避bug
+            service = Service(globalparam.linux_driver_path)
+            dr = webdriver.Chrome(options=options, service=service)
+        elif browser == "edge":
+            dr = webdriver.Edge()
+        else:
+            raise UnsupportedBrowserException(f"Browser {browser} is not supported.")
+        return dr
     else:
-        raise UnsupportedBrowserException(f"Browser {browser} is not supported.")
-    return dr
+        raise UnsupportedBrowserException(f"System {system_driver} is not supported.")
 
