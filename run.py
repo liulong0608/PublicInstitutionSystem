f884724e2b0b9673be0d6edfb36ba88b5657f8f1
Echo <liulong@3mencn.com>
2024-01-03 13:47:47 +0800

    v1.3.6-beta1.3.5 refactor: 更改测试报告样式

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ import unittest
 from utils.HTMLTestRunner import *
 import time
 from config import globalparam
+from BeautifulReport import BeautifulReport as bf
 
 
 def run():
@@ -19,13 +20,16 @@ def run():
     now = time.strftime('%Y-%m-%d')
     # reportname = globalparam.report_path + '\\' + 'TestResult' + now + '.html'
     reportname = os.path.join(globalparam.report_path, 'report.html')
-    with open(reportname, 'w', encoding='utf-8') as f:
-        runner = HTMLTestRunner(
-            stream=f, verbosity=2,
-            title='测试报告',
-            description='Test the import testcase'
-        )
-        runner.run(suite)
+    run_result = bf(suite)
+    run_result.report(filename=reportname, description='测试报告',
+                      report_dir=globalparam.report_path)
+    # with open(reportname, 'w', encoding='utf-8') as f:
+    #     runner = HTMLTestRunner(
+    #         stream=f, verbosity=2,
+    #         title='测试报告',
+    #         description='Test the import testcase'
+    #     )
+    #     runner.run(suite)
     # time.sleep(3)
     # # 发送邮件
     # mail = sendmail.SendMail()
