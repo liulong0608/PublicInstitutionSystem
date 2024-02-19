f8fa33425dda0331edc9bad93c1e5a8a54a3b624
Echo <liulong@3mencn.com>
2024-02-19 11:28:09 +0800

    v1.5.02 fix: 修复选择性别报错问题

--- testcase/test_create_unit.py
+++ testcase/test_create_unit.py
@@ -27,6 +27,6 @@ class TestCreateUnit(BaseUtil):
         self.assert_text(save_msg, '保存成功，是否需要维护单位信息？')
         maintenance_msg = cu.maintenance_unit_information()
         self.assert_text(maintenance_msg, '保存成功，稍后请在单位信息查看')
-        operator_msg = cu.create_operator(username=orgcode, gender=random.choice(['男', '女']),
+        operator_msg = cu.create_operator(username=orgcode, gender="男",
                                           phoneNumber="15112345678", identityCard=generate_idcard())
         self.assert_text_contains(operator_msg, '用户已添加')
